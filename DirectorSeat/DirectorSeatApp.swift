d1aea6e4135b01900374b7a770c1bac2c00a12a7
egehazar <ege.hazar42@gmail.com>
2026-04-24 19:33:45 +0300

    Initial Commit

---
+++ DirectorSeat/DirectorSeatApp.swift
@@ -0,0 +1,17 @@
+//
+//  DirectorSeatApp.swift
+//  DirectorSeat
+//
+//  Created by Ege Hazar on 24.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct DirectorSeatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
